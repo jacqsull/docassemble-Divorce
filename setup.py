695debcadfd357f650c7c42c77edabbeef953c40
Jacqueline Sullivan <jacqsull@legalservicesma.org>
2019-04-12 15:27:58 +0000

    More changes to support and checkboxes

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.Divorce',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description=u'# docassemble.Divorce\n\nA docassemble extension.\n\n## Author\n\nJacqueline Sullivan, jacqsull@legalservicesma.org\n\n',
+      long_description=u'# docassemble.Divorce\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nJacqueline Sullivan, jacqsull@legalservicesma.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Jacqueline Sullivan',
       author_email='jacqsull@legalservicesma.org',
